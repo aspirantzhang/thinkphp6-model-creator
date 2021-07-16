15231ac66d1a220a674f62d8705d8010d4eab635
aspirantzhang <admin@aspirantzhang.com>
2021-07-17 00:31:52 +0800

    ci: change db config position in unit test

--- tests/DbTest.php
+++ tests/DbTest.php
@@ -13,36 +13,6 @@ class DbTest extends TestCase
 {
     public static function setUpBeforeClass(): void
     {
-        ThinkDb::setConfig([
-            // 默认数据连接标识
-            'default'     => 'mysql',
-            // 数据库连接信息
-            'connections' => [
-                'mysql' => [
-                    // 数据库类型
-                    'type'     => 'mysql',
-                    // 主机地址
-                    'hostname' => '127.0.0.1',
-                    // 数据库名
-                    'database' => 'model_creator',
-                    // 用户名
-                    'username' => 'root',
-                    // 密码
-                    'password' => 'dbpass',
-                    // 数据库编码默认采用utf8
-                    'charset'  => 'utf8',
-                    // 数据库表前缀
-                    'prefix'   => '',
-                    // 是否需要断线重连
-                    'break_reconnect' => false,
-                    // 断线标识字符串
-                    'break_match_str' => [],
-                    // 数据库调试模式
-                    'debug'    => false,
-                ],
-            ],
-        ]);
-        
         ThinkDb::execute('DROP TABLE IF EXISTS `auth_rule`, `auth_rule_i18n`, `menu`, `menu_i18n`, `auth_group_rule`, `unit-test`, `unit-test_i18n`, `unit-test-2`, `unit-test-2_i18n`;');
         ThinkDb::execute(<<<END
 CREATE TABLE `auth_rule` (
