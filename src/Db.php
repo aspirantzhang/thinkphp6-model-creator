f4bfd6c145a406b10b408c958c557379b7c17240
aspirantzhang <admin@aspirantzhang.com>
2021-07-23 23:12:35 +0800

    feat(db): add fieldsHandler

--- src/Db.php
+++ src/Db.php
@@ -198,4 +198,79 @@ class Db
         }
         return $childrenIds;
     }
+
+    private function getExistingFields(string $tableName)
+    {
+        $existingFields = [];
+        $columnsQuery = ThinkDb::query("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE table_name = :tableName;", ['tableName' => $tableName]);
+        if ($columnsQuery) {
+            $existingFields = extractValues($columnsQuery, 'COLUMN_NAME');
+        }
+        return $existingFields;
+    }
+ 
+    public function fieldsHandler(array $processedFields, array $fieldsData, array $reservedFields)
+    {
+        $existingFields = $this->getExistingFields($this->tableName);
+        // group by types
+        $delete = array_diff($existingFields, $processedFields);
+        $add = array_diff($processedFields, $existingFields);
+        $change = array_intersect($processedFields, $existingFields);
+
+        $statements = [];
+        foreach ($fieldsData as $field) {
+            switch ($field['type']) {
+                case 'longtext':
+                    $type = 'LONGTEXT';
+                    $typeAddon = '';
+                    $default = 'DEFAULT \'\'';
+                    break;
+                case 'number':
+                    $type = 'INT';
+                    $typeAddon = ' UNSIGNED';
+                    $default = 'DEFAULT 0';
+                    break;
+                case 'datetime':
+                    $type = 'DATETIME';
+                    $typeAddon = '';
+                    break;
+                case 'tag':
+                case 'switch':
+                    $type = 'TINYINT';
+                    $typeAddon = '(1)';
+                    $default = 'DEFAULT 1';
+                    break;
+                default:
+                    $type = 'VARCHAR';
+                    $typeAddon = '(255)';
+                    $default = 'DEFAULT \'\'';
+                    break;
+            }
+
+            if (in_array($field['name'], $add)) {
+                $method = 'ADD';
+                $statements[] = " $method `${field['name']}` $type$typeAddon NOT NULL $default";
+            }
+
+            if (in_array($field['name'], $change)) {
+                $method = 'CHANGE';
+                $statements[] = " $method `${field['name']}` `${field['name']}` $type$typeAddon NOT NULL $default";
+            }
+        }
+
+        foreach ($delete as $field) {
+            $method = 'DROP IF EXISTS';
+            if (!in_array($field, $reservedFields)) {
+                $statements[] = " $method `$field`";
+            }
+        }
+
+        $alterTableSql = 'ALTER TABLE `' . $this->tableName . '` ' . implode(',', $statements) . ';';
+
+        try {
+            ThinkDb::query($alterTableSql);
+        } catch (\Exception $e) {
+            throw new \Exception(__('change table structure failed', ['tableName' => $this->tableName]));
+        }
+    }
 }
