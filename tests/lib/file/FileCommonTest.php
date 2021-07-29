aa3974ffac28d5c0aef418275197c677fdb7e144
aspirantzhang <admin@aspirantzhang.com>
2021-07-29 14:48:15 +0800

    refactor: replan FileCommon

--- tests/lib/file/FileCommonTest.php
+++ tests/lib/file/FileCommonTest.php
@@ -6,17 +6,17 @@ namespace aspirantzhang\octopusModelCreator\lib\file;
 
 use think\Exception;
 
-class BasicModelTest extends \aspirantzhang\octopusModelCreator\TestCase
+class FileCommonTest extends \aspirantzhang\octopusModelCreator\TestCase
 {
-    protected $basicModel;
+    protected $fileCommon;
     protected $basePath;
     protected $runtimePath;
 
     protected function setUp(): void
     {
-        $this->basePath = createPath(dirname(__DIR__, 2), 'stubs', 'lib', 'file', 'BasicModel', '');
-        $this->runtimePath = createPath(dirname(__DIR__, 3), 'runtime', 'file', 'BasicModel', '');
-        $this->basicModel = new BasicModel();
+        $this->basePath = createPath(dirname(__DIR__, 2), 'stubs', 'lib', 'file', 'FileCommon', '');
+        $this->runtimePath = createPath(dirname(__DIR__, 3), 'runtime', 'file', 'FileCommon', '');
+        $this->fileCommon = new FileCommon();
         parent::setUp();
     }
 
@@ -25,13 +25,13 @@ class BasicModelTest extends \aspirantzhang\octopusModelCreator\TestCase
         $sourcePath = $this->basePath . 'replaceAndWrite.stub';
         $snapshotPath = $this->basePath . 'replaceAndWrite.snap';
         $targetPath = $this->runtimePath . 'replaceAndWrite.php';
-        $this->basicModel->replaceAndWrite($sourcePath, $targetPath, function ($content) {
+        $this->fileCommon->replaceAndWrite($sourcePath, $targetPath, function ($content) {
             return strtr($content, ['{%a%}' => 'foo','{%b%}' => 'bar']);
         });
         $this->assertTrue(matchSnapshot($targetPath, $snapshotPath));
 
         try {
-            $this->basicModel->replaceAndWrite($sourcePath . '.notExist', $targetPath, function () {
+            $this->fileCommon->replaceAndWrite($sourcePath . '.notExist', $targetPath, function () {
             });
         } catch (Exception $e) {
             $this->assertEquals($e->getMessage(), 'unable to get file content: filePath=' . createPath($sourcePath . '.notExist'));
