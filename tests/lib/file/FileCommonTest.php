fea10d9aaebde1a8e997e2650c8f6ac924587162
aspirantzhang <admin@aspirantzhang.com>
2021-07-30 14:47:41 +0800

    test: add fieldsData property to FIleCommonTest

--- tests/lib/file/FileCommonTest.php
+++ tests/lib/file/FileCommonTest.php
@@ -11,6 +11,86 @@ class FileCommonTest extends \aspirantzhang\octopusModelCreator\TestCase
     protected $fileCommon;
     protected $basePath;
     protected $runtimePath;
+    protected $fieldsData = [
+        [
+            "name" => "nickname",
+            "title" => "Nick Name",
+            "type" => "input",
+            "settings" => [
+                "validate" => [
+                    "require",
+                    "length"
+                ],
+                "options" => [
+                    "length" => [
+                        "min" => 4,
+                        "max" => 32
+                    ]
+                ]
+            ],
+            "allowHome" => true,
+            "allowRead" => true,
+            "allowSave" => true,
+            "allowUpdate" => true,
+            "allowTranslate" => true
+        ],
+        [
+            "name" => "gender",
+            "title" => "Gender",
+            "type" => "radio",
+            "data" => [
+                [
+                    "title" => "Mx",
+                    "value" => "mx"
+                ],
+                [
+                    "title" => "Mr",
+                    "value" => "mr"
+                ],
+                [
+                    "title" => "Ms",
+                    "value" => "ms"
+                ]
+            ],
+            "settings" => [
+                "validate" => [
+                    "require"
+                ]
+            ],
+            "allowHome" => true,
+            "allowRead" => true,
+            "allowSave" => true,
+            "allowUpdate" => true
+        ],
+        [
+            "name" => "married",
+            "title" => "Married",
+            "type" => "switch",
+            "hideInColumn" => true,
+            "data" => [
+                [
+                    "title" => "Yes",
+                    "value" => 1
+                ],
+                [
+                    "title" => "No",
+                    "value" => 0
+                ]
+            ],
+            "settings" => [
+                "display" => [
+                    "listSorter"
+                ],
+                "validate" => [
+                    "require"
+                ]
+            ],
+            "allowHome" => true,
+            "allowRead" => true,
+            "allowUpdate" => true,
+            "allowSave" => true
+        ]
+    ];
 
     protected function setUp(): void
     {
